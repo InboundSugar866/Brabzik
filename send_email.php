12a8a859d551e579b441050f4d3f7901da25ba18
InboundSugar866 <146993818+InboundSugar866@users.noreply.github.com>
2024-03-28 08:24:39 +0100

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,74 @@
+<?php
+
+header('Allow: GET, POST, HEAD');
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
+$email_from = 'brabzik.ensem@gmail.com';//<== update the email address
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+    "Here is the message:\n $message".
+    
+$to = "brabzik.ensem@gmail.com";//<== update the email address
+$headers = "From: $email_from \r\n";
+$headers .= "Reply-To: $visitor_email \r\n";
+//Send the email!
+mail($to,$email_subject,$email_body,$headers);
+//done. redirect to thank-you page.
+header('Location: thank-you.html');
+
+
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
+} 
+/*
+$to = 'recipient@example.com';
+$subject = 'Test Email';
+$message = 'This is a test email sent from PHP.';
+$headers = 'From: noreply@example.com';
+
+if (mail($to, $subject, $message, $headers)) {
+    echo 'Email sent successfully!';
+} else {
+    echo 'Error sending email. Check your configuration.';
+}
+*/
+?>
